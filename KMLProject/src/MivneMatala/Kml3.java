07f32f96cf6a4f7db080374f42446610d431bc3b
Oriel <33528173+Xcompanygames@users.noreply.github.com>
2017-12-27 21:43:48 +0200

    wrf

--- KMLProject/src/MivneMatala/Kml3.java
+++ KMLProject/src/MivneMatala/Kml3.java
@@ -1,5 +1,46 @@
 package MivneMatala;
 
-public class Kml3 {
 
-}
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+import de.micromata.opengis.kml.v_2_2_0.Document;
+import de.micromata.opengis.kml.v_2_2_0.Kml;
+import de.micromata.opengis.kml.v_2_2_0.KmlFactory;
+import de.micromata.opengis.kml.v_2_2_0.Placemark;
+import de.micromata.opengis.kml.v_2_2_0.TimeStamp;
+
+	public class Kml3 {
+		private static Integer ID=0;
+		// Function to cast Double to string
+	
+			public static void ToKml(FileFiltered InfoByInput){	
+				final Kml kml = new Kml();
+				Document document = kml.createAndSetDocument();
+
+				for (int i = 0; i < InfoByInput.getIDList().size(); i++) {
+					ID++;
+					String time=InfoByInput.getTIMEList().get(i);
+					System.out.println(InfoByInput.getTIMEList().get(i));
+					Timestamp t=Timestamp.valueOf(time);
+					Placemark p=KmlFactory.createPlacemark().withName("Placemark"+i+" "+InfoByInput.getIDList().get(i));
+					p.createAndSetTimeStamp().addToTimeStampSimpleExtension(t);
+					document.createAndAddPlacemark().withOpen(Boolean.TRUE).withTimePrimitive(p.getTimePrimitive()).createAndSetPoint()
+					.addToCoordinates(InfoByInput.getLONList().get(i),InfoByInput.getLATList().get(i));
+				}
+
+				try {
+					kml.marshal(new File("C:/Mivne/KML"+ID+".kml"));
+					System.out.println("completed");
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	
