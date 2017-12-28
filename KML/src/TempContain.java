3ddc82f2faed8cf6c6a94c6bbd79990addcab18c
pikachu1990 <Moshe@Moses-PC>
2017-12-28 06:24:57 +0200

    Alg2update

---
+++ KML/src/TempContain.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+public class TempContain implements Comparable<TempContain> {
+
+	private int CHANNEL,SIGNAL;
+	private String SSID,MAC;
+	
+	public TempContain(String ssid,String mac,int channel,int signal) {
+		
+		this.SSID = ssid + "";
+		this.MAC = mac + "";
+		this.CHANNEL = channel;
+		this.SIGNAL = signal;
+		
+	}
+	
+    public int compareTo(TempContain o) {
+
+        return this.SIGNAL-o.SIGNAL;
+        
+    }
+
+	public int getCHANNEL() {
+		return CHANNEL;
+	}
+
+	public void setCHANNEL(int cHANNEL) {
+		CHANNEL = cHANNEL;
+	}
+
+	public int getSIGNAL() {
+		return SIGNAL;
+	}
+
+	public void setSIGNAL(int sIGNAL) {
+		SIGNAL = sIGNAL;
+	}
+
+	public String getSSID() {
+		return SSID;
+	}
+
+	public void setSSID(String sSID) {
+		SSID = sSID;
+	}
+
+	public String getMAC() {
+		return MAC;
+	}
+
+	public void setMAC(String mAC) {
+		MAC = mAC;
+	}
+    
+    
+    
+}
---
+++ KML/src/Writer.java
@@ -0,0 +1,47 @@
+
+//not mine all good.
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+/**
+ * Input : Filename and Location
+ * Return : Nothing - Just prints the file in the location creating a File.
+ */
+public class Writer {
+	
+	public Writer(String Filename,ArrayList<String> Source) {
+		Write(Filename,Source);
+	}
+
+	public void Write(String fileName,ArrayList<String> Source) {// Method that Write into the file the Finished Data	                            
+        try {
+            // Assume default encoding.
+            FileWriter fileWriter =
+                new FileWriter(fileName);
+
+            // Always wrap FileWriter in BufferedWriter.
+            BufferedWriter bufferedWriter =
+                new BufferedWriter(fileWriter);
+
+            // Note that write() does not automatically
+            // append a newline character.
+            	 for (int i=0;i<Source.size();i++) {
+                    	bufferedWriter.write(Source.get(i));
+                    	bufferedWriter.newLine();
+            	 }
+                   		                 		                        		                		                    
+         // Always close files.
+            bufferedWriter.close();
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error writing to file '"
+                + fileName + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
+
+        
+}// Makes an array of all the latency numbers
+}
